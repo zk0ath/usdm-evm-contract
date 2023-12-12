076670f5cd9d595c018a4ca0012b54040866d5f9
Caner Yakupoğlu <caneryakupoglu@outlook.com>
2023-12-12 05:02:56 +0300

    Change deploy flow. compiler version change.

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,9 +1,13 @@
 const MyUsdc = artifacts.require("MyUsdc");
 const ReceiveUSDC = artifacts.require("ReceiveUSDC");
 
+require('dotenv').config();
+const { ADDRESS } = process.env;
+
 module.exports = async function (deployer) {
 
-  var tx = await deployer.deploy(MyUsdc, "0xafd7a5b2E91F8b7A0324cC0a5fd18827c476dDB0");
-  console.log(tx)
-  deployer.deploy(ReceiveUSDC, "0x07865c6E87B9F70255377e024ace6630C1Eaa37F");
+  await deployer.deploy(MyUsdc, ADDRESS);
+  await MyUsdc.deployed()
+
+  await deployer.deploy(ReceiveUSDC, MyUsdc.address);
 };
