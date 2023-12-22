ab33e742c42e36a588a331baaa718213b9154e69
Caner Yakupoğlu <caneryakupoglu@outlook.com>
2023-12-22 20:16:49 +0300

    add tests for releaseTokens function.

--- test/BridgeTest.js
+++ test/BridgeTest.js
@@ -30,4 +30,25 @@ contract("Bridge", accounts => {
         assert.equal(transfer.completed, true, "The transfer should be marked as completed");
     });
 
+    it("should release USDC tokens to the recipient", async () => {
+        await usdcMock.mint(bridge.address, web3.utils.toBN("1000000"));
+
+        const transactionId = 1;
+        await bridge.releaseTokens(user, "1000", transactionId, { from: owner });
+
+        const userBalance = await usdcMock.balanceOf(user);
+        assert.equal(userBalance.toString(), "1000", "The user should receive 1000 USDC tokens");
+    });
+
+    it("should not allow the same transaction to be processed twice", async () => {
+        const transactionId = 1;
+        try {
+            await bridge.releaseTokens(user, "1000", transactionId, { from: owner });
+            assert.fail("The same transaction should not be processed twice");
+        } catch (error) {
+            assert(error, "Expected an error but did not get one");
+            assert(error.message.includes("Transaction already processed"), "Expected 'Transaction already processed' error");
+        }
+    });
+
 });
